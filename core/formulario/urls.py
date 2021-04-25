fc68cea50178252fd4e3b1eb0f45920bb45294db
Carlos_Osuna <carlosalvaroosuna1@gmail.com>
2021-04-25 19:23:43 -0400

    version 1

---
+++ core/formulario/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from core.formulario.views import *
+urlpatterns = [
+    path('',formulario,name='formulario'),
+    path('reportar',respuesta,name='reportar_seleccion')   
+]
---
+++ core/formulario/views.py
@@ -0,0 +1,37 @@
+import http
+from django.http.response import HttpResponse, JsonResponse,HttpResponseNotFound
+from django.shortcuts import render
+from django.templatetags.static import static
+import json
+
+def formulario(request):
+    args= {
+        'titulo':'Formulario Reportes EconomiaDigital',
+        'titulo_form': 'Por favor, elija 3 activos a reportar'
+    }
+    activos = open( 'core/formulario'+ static('formulario/files/activos.txt'),'r')
+    lista = list(map(lambda x: x.rstrip('\n'),activos.readlines()))
+    activos.close()
+    args['activos'] = lista
+    # return HttpResponseNotFound("<h1>Pagina No disponible</h1>")
+    return render(request,'formulario/formulario.html',args)
+
+def respuesta(request):
+
+    archivo = open( 'core/formulario'+ static('formulario/files/resultados.json'),"r")
+    data = json.load(archivo)
+    archivo.close()
+    activos = json.loads(request.body)['activos']
+    for activo in activos:
+        if activo in data.keys():
+            data[activo] += 1
+        else:
+            data[activo] = 1
+    archivo = open( 'core/formulario'+ static('formulario/files/resultados.json'),'w')
+    json.dump(data, archivo,indent=4)
+    archivo.close()
+    data = {
+        'ok':'ok'
+    }
+    return JsonResponse(data,status = http.HTTPStatus.OK)
+
