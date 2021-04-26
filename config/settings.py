1d797d5caf58112f1cdfaf64c120c590d7c5b44c
Carlos_Osuna <carlosalvaroosuna1@gmail.com>
2021-04-25 20:10:46 -0400

    a

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -122,4 +121,3 @@ STATICFILES_DIRS=[
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
